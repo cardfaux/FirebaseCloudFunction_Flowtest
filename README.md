702c249ceca69355de596c3979bedc564ced9cc9
James Hagood <cardfaux@tutanota.com>
2023-03-31 13:53:21 -0500

    added a .env file and moved everything into try catch blocks

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
 # FlowTest Cloud Function for the FlowTest Firebase Project
 
 ### I set this up so i could have more control over the response
+
+<!-- example of looping over users from Dimitar -->
+
+```
+const snapshot = await usersCollection.get();
+
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+      // Perform your desired operation with the document data
+    });
+
+```
+
+<!-- example of using a callable function in a custom action/function from Dimitar -->
+
+```
+final functions = CloudFunctions.instance;
+  final myCallableFunction = functions.getHttpsCallable(
+    functionName: 'myCallableFunction',
+  );
+try {
+    final response = await myCallableFunction.call(
+      <String, dynamic>{
+        'inputValue': 'Your input value goes here',
+      },
+    );
+
+    print('Function result: ${response.data['result']}');
+  } catch (e) {
+    print('Error calling function: $e');
+  }
+
+```
+
+<!-- example of writing a scheduled function from Dimitar -->
+
+```
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.updateUserNames = functions.pubsub.schedule('30 * * * *').onRun(async (context) => {
+  // Get a reference to the users collection
+  const usersCollection = admin.firestore().collection('users');
+
+  // Generate a new name (you can replace this with your desired logic)
+  const newName = `New Name ${Date.now()}`;
+
+  // Get all user documents
+  const snapshot = await users
+
+```
+
+<!-- example of a cloud function from https://www.freecodecamp.org/news/how-to-schedule-a-task-with-firebase-cloud-functions/ -->
+
+```
+const functions = require("firebase-functions");
+const firebase = require("firebase-admin");
+firebase.initializeApp()
+var firestore = firebase.firestore()
+
+exports.resetCreditsForFreeUsers = functions.pubsub
+  .schedule('0 0 1 * *')
+  .onRun(async (context) => {
+    const users = firestore.collection('users')
+    const user = await users.where('isPayingUser', '==', false).get()
+    user.forEach(snapshot => {
+        snapshot.ref.update({ credits: 10 })
+    })
+    return null;
+})
+
+```
+
+<!-- youtube link from Dimitar -->
+
+https://www.youtube.com/watch?v=x-kivZ7ChD8&t=1s
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
+const moment = require('moment');
+const firebase = require('firebase-admin');
+firebase.initializeApp();
+var firestore = firebase.firestore();
 //const { fetch } = require('node-fetch');
 
 // Create and deploy your first functions
@@ -43,6 +47,117 @@ exports.helloWorld = functions.https.onRequest(async (request, response) => {
     response.json(responseData);
   }
   //response.json(responseData);
+});
+
+exports.updateFirebaseUsers = functions.pubsub.schedule('*/30 * * * *').onRun(async (context) => {
+  //! this is where i want it to work start
+  let n = new Date();
+  //n.toISOString();
+  let onlyDateCurrent = moment(n.toISOString()).format('YYYY-MM-DD');
+  // let onlyHoursCurrent = moment(n.toISOString()).format('HH:mm:ss');
+
+  // let onlyDateMinus30 = moment(n.toISOString()).subtract(30, 'minutes').format('YYYY-MM-DD');
+  // let onlyHoursMinus30 = moment(n.toISOString()).subtract(30, 'minutes').format('HH:mm:ss');
+  const users = firestore.collection('users');
+  const user = await users.where('employee_role_name', '==', 'Associate').get();
+  //const salesTotal = 10.92 + `${Date.now()}`;
+  //const salesTotal = `${salesTotalsSummed.toString()}` + `${Date.now()}`;
+  user.forEach(async (snapshot) => {
+    const { employee_id } = snapshot.data();
+    //! This is the where the access token gets updated.
+    const updateTokenResponse = await fetch('https://cloud.lightspeedapp.com/oauth/access_token.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: process.env.REFRESH_TOKEN,
+        grant_type: 'refresh_token',
+      }),
+    });
+    const updateTokenData = await updateTokenResponse.json();
+    const newUpdatedToken = updateTokenData.access_token;
+    //! This where the API call is made to get the total sales.
+    const salesDataResponse = await fetch(
+      `${process.env.BASE_URL}/${process.env.ACCOUNT_ID}/Sale.json?timeStamp=%3E,${onlyDateCurrent}T00:00:00-0400&employeeID=${employee_id}&sort=-timeStamp&load_relations=all`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${newUpdatedToken}`,
+        },
+      }
+    );
+    const salesData = await salesDataResponse.json();
+    //! if no Sale data is returned, return null.
+    if (!salesData.Sale) {
+      try {
+        console.log('IN THE !salesData.Sale BLOCK', salesData);
+        return null;
+      } catch (error) {
+        console.log('ERROR FROM THE !salesData.Sale BLOCK', error);
+        return null;
+      }
+    } else if (salesData.Sale) {
+      try {
+        const salesTotals = salesData.Sale.map((sale) => parseFloat(sale.total));
+        const totalsArray = [];
+        salesTotals.forEach((total) => totalsArray.push(total));
+        const salesTotalsSummed = totalsArray.reduce((total, sale) => total + sale, 0);
+        const salesTotalTicketAverage = salesTotalsSummed / salesTotals.length;
+        console.log('salesTotalsSummed', salesTotalsSummed);
+        functions.logger.info('salesTotalsSummed', salesTotalsSummed, { structuredData: true });
+        const salesTotalSummedRoundedTenth = salesTotalsSummed.toFixed(2);
+        const salesTotalSummedAsADouble = parseFloat(salesTotalSummedRoundedTenth);
+        const salesTotalTicketAverageRoundedTenth = salesTotalTicketAverage.toFixed(2);
+        snapshot.ref.update({ sales_total: salesTotalSummedRoundedTenth });
+        snapshot.ref.update({ sales_total_ticket_average: salesTotalTicketAverageRoundedTenth });
+        snapshot.ref.update({ total_sort: salesTotalSummedAsADouble });
+        console.log('IN THE ELSE IF BLOCK, SUMMED-DOUBLE', salesTotalSummedAsADouble);
+        return null;
+      } catch (error) {
+        console.log('ERROR FROM THE ELSE IF BLOCK', error);
+        return null;
+      }
+    } else {
+      console.log('FROM THE ELSE BLOCK');
+      return null;
+    }
+    // if (salesData.Sale) {
+    //   const salesTotals = salesData.Sale.map((sale) => parseFloat(sale.total));
+    //   const totalsArray = [];
+    //   salesTotals.forEach((total) => totalsArray.push(total));
+    //   const salesTotalsSummed = totalsArray.reduce((total, sale) => total + sale, 0);
+    //   const salesTotalTicketAverage = salesTotalsSummed / salesTotals.length;
+    //   console.log('salesTotalsSummed', salesTotalsSummed);
+    //   functions.logger.info('salesTotalsSummed', salesTotalsSummed, { structuredData: true });
+    //   const salesTotalSummedRoundedTenth = salesTotalsSummed.toFixed(2);
+    //   const salesTotalSummedAsADouble = parseFloat(salesTotalSummedRoundedTenth);
+    //   const salesTotalTicketAverageRoundedTenth = salesTotalTicketAverage.toFixed(2);
+    //   // console.log('OPEN SALESDATA', salesData);
+    //   // console.log('SALES TOTAL DOUBLE', salesTotalSummedAsADouble);
+    //   // functions.logger.info('OPEN SALESDATA', salesData, { structuredData: true });
+    //   // functions.logger.info('SALES TOTAL DOUBLE', salesTotalSummedAsADouble, { structuredData: true });
+    //   snapshot.ref.update({ sales_total: salesTotalSummedRoundedTenth });
+    //   snapshot.ref.update({ sales_total_ticket_average: salesTotalTicketAverageRoundedTenth });
+    //   snapshot.ref.update({ total_sort: salesTotalSummedAsADouble });
+    // }
+    //snapshot.ref.update({ sales_total: salesTotalsSummed });
+    //snapshot.ref.update({ sales_updated_employee_id: employee_id });
+  });
+  console.log('FROM THE END OF THE FUNCTION OUTSIDE THE USERS COLLECTION LOOP');
+  return null;
+  //! this is where i want it work end
+  // const users = firestore.collection('users');
+  // const user = await users.where('employee_id', '==', 469).get();
+  // //const salesTotal = 10.92 + `${Date.now()}`;
+  // const salesTotal = `${salesTotalsSummed}` + `${Date.now()}`;
+  // user.forEach((snapshot) => {
+  //   snapshot.ref.update({ sales_total: salesTotal });
+  // });
+  // return null;
 });
 
 //Callable function.
